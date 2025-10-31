f36df3ad28abd87fb2f75d45f0445e8ed0ece6df
Amanda <syukay00789@gmail.com>
2025-10-31 23:19:50 +1100

    TavilySearch

--- main.py
+++ main.py
@@ -2,37 +2,36 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-from langchain.agents import create_agent
+from langchain_classic import hub
+from langchain_classic.agents import AgentExecutor
+from langchain_classic.agents.react.agent import create_react_agent
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableLambda
 from langchain_openai import ChatOpenAI
 from langchain_tavily import TavilySearch
 
-from schemas import AgentResponse
-
 tools = [TavilySearch()]
-llm = ChatOpenAI(model="gpt-4o")
-
-
-agent = create_agent(
-    model=llm,
+llm = ChatOpenAI(model="gpt-4")
+react_prompt = hub.pull("hwchase17/react")
+agent = create_react_agent(
+    llm=llm,
     tools=tools,
-    response_format=AgentResponse,
+    prompt=react_prompt,
 )
 
 
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+chain = agent_executor
+
 def main():
-    result = agent.invoke(
-        {
-            "messages": [
-                {
-                    "role": "user",
-                    "content": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
-                }
-            ]
+    result = chain.invoke(
+        input={
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
-    # Access structured response from the agent
-    structured = result.get("structured_response", None)
-    print(structured if structured is not None else result)
+    print(result)
+    print("Hello from langchain-course!")
 
 
 if __name__ == "__main__":
